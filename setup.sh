b4d5c0cc6caa13d6ee32a064e30b43bdaddddaa6
Lim Yue Chuan <shasderias@gmail.com>
2015-07-14 10:03:40 +0800

    update setup.sh for Vundle's new interface

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p ~/.vimtemp/
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/
